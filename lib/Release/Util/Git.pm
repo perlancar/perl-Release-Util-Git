c01c67ad9aa07ef6ebe75d7ee165a78c5a624e1c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2017-02-10 08:39:39 +0700

    Add list_git_release_years()

--- lib/Release/Util/Git.pm
+++ lib/Release/Util/Git.pm
@@ -77,6 +77,44 @@ sub list_git_release_tags {
     [200, "OK", \@res, $resmeta];
 }
 
+$SPEC{list_git_release_years} = {
+    v => 1.1,
+    summary => 'List git release years',
+    description => <<'_',
+
+This routine uses list_git_release_tags() to collect the release tags and their
+dates, then group them by year.
+
+_
+    args => $SPEC{list_git_release_tags}{args},
+    deps => $SPEC{list_git_release_tags}{deps},
+    examples => [
+        {
+            args => {detail=>1, regex=>'^release'},
+            'x.doc.show_result' => 0,
+            test => 0,
+        },
+    ],
+};
+sub list_git_release_years {
+    my %args = @_;
+    my $res = list_git_release_years(%args);
+    return $res unless $res->[0] == 200;
+
+    my %tags; # key = year
+    my $resmeta = {};
+
+    my @res;
+    if ($args{detail}) {
+        @res = map { +{year=>$_, tags=>$tags{$_}} } reverse sort keys %tags;
+        $resmeta->{'table.fields'} = [qw/year tags/];
+    } else {
+        @res = reverse sort keys %tags;
+    }
+
+    [200, "OK", \@res, $resmeta];
+}
+
 1;
 # ABSTRACT: Utility routines related to software releases and git
 
